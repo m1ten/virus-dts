ce9b1abfcd3e748dff87dd90b34a538422ed717f
m1ten <57693631+m1ten@users.noreply.github.com>
2020-07-16 18:26:35 -0400

    shark

--- shark.py
+++ shark.py
@@ -3,80 +3,85 @@ import os
 import json
 from discord.ext import commands
 
-# data variable
-data = {
-    "bot": {
-        "token": None,
-        "owner_id": None
-    },
-    "guild": {},
-    "user": {}
+# variables
+bot = {
+    "name": "shark",
+    "token": None,
+    "owner_id": None
 }
+guild = {}
+user = {}
 
 
 # initialize setup
-def _data_():
-    global data
+def _bot_():
+    global bot
 
-    # load data from data.json
-    read_data = open('./data.json', 'r')
-    data = json.load(read_data)
-    read_data.close()
+    # load data from bot.json
+    read_bot = open('./data/bot.json', 'r')
+    bot = json.load(read_bot)
+    read_bot.close()
 
-    # open data.json with write permission
-    write_data = open('./data.json', 'w')
+    # open bot.json with write permission
+    write_bot = open('./data/bot.json', 'w')
 
     # if there is no token, input token
-    if data['token'] is None:
+    if bot['token'] is None:
         data['token'] = input('Enter Bot Token: ')
 
     # if there is no owner_id, input owner_id
-    if data['owner_id'] is None:
+    if bot['owner_id'] is None:
         data['owner_id'] = int(input('Enter Owner ID: '))
 
-    # dump data to data.json
-    json.dump(data, write_data, indent=4)
-    write_data.close()
+    # dump data to bot.json
+    json.dump(bot, write_bot, indent=4)
+    write_bot.close()
 
-    return data
+    return bot
+
+def json_load(file):
+    return json.load(open(file, 'r'))
+
+def json_dump(variable, file, indent=4):
+    return json.dump(variable, open(file, 'w'), indent=indent)
+
 
 
 # get prefix from data
 async def get_prefix(bot, ctx):
-    data = _data_()
+    guild = json_load('./data/guild.json')
+    user = json_load('./data/user.json')
 
-    try:
-        return data[str(ctx.guild.id)]["prefix"]
-    except:
-        return '!'  # if no prefix assigned or dm
+    try: return guild[str(ctx.guild.id)]["prefix"]
+    
+    try: return user[str(ctx.guild.id)]["prefix"]
+
+    return "!"
 
 # define bot, assign prefix
-bot = commands.Bot(command_prefix=get_prefix)
+client = commands.Bot(command_prefix=get_prefix)
 
 
 @bot.event
-async def on_ready():
+async def on_ready(client):
     # set status and activity of bot
-    await bot.change_presence(status=discord.Status.idle,
+    await client.change_presence(status=discord.Status.idle,
                               activity=discord.Game('! | m1ten.me/shark'))
 
-    data = _data_()
-    bot.owner_id = data['owner_id']
+    shark = json_load('./data/bot.json')
+    client.owner_id = shark['owner_id']
 
-    try:
-        await bot.get_user(bot.owner_id).send('Shark is online.')
-    except:
-        pass
+    try: await client.get_user(bot.owner_id).send('Shark is online.')
 
-    print('Shark is online.')
+    print(f'{bot.name} is online.')
 
     return
 
 if __name__ == '__main__':
-    _data_()
+    _bot_()
 
     for cog_files in os.listdir('commands'):
         if cog_files.endswith('.py') and not cog_files.startswith('_'):
             bot.load_extension(f'commands.{cog_files[:-3]}')
 
-bot.run(data['token'])
+client.run(bot['token'])
